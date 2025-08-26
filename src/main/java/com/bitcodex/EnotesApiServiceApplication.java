2d71fdeecdeed4c28463b972997e09227b287efa
Tasneem Ahmad Ansari <www.ahmad.tasneem@gmail.com>
2025-08-26 21:53:17 +0530

    initial commit

---
+++ src/main/java/com/bitcodex/EnotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bitcodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServiceApplication.class, args);
+	}
+
+}
