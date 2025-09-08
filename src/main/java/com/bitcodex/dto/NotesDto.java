7ee6c6f2576bb79d74a318ef04d3118979b2a8fa
Tasneem Ahmad Ansari <www.ahmad.tasneem@gmail.com>
2025-09-08 22:24:09 +0530

    save note impl

---
+++ src/main/java/com/bitcodex/dto/NotesDto.java
@@ -0,0 +1,41 @@
+package com.bitcodex.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NotesDto {
+	
+	private Integer id;
+	
+	private String title;
+	
+	private String description;
+	
+	private CategoryDto category;
+	
+	private Integer createdBy;
+	
+	private Date createdOn;
+	
+	private Integer updatedBy;
+	
+	private Date updatedOn;
+	
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	public static class CategoryDto{
+		private Integer id;
+		
+		private String name;
+	}
+}
