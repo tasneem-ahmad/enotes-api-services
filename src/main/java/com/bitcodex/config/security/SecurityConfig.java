400a5cc607a02c89e4a5ffc6a4f197f13e2da9a5
Tasneem Ahmad Ansari <www.ahmad.tasneem@gmail.com>
2025-09-24 08:42:39 +0530

    token impl

--- src/main/java/com/bitcodex/config/security/SecurityConfig.java
+++ src/main/java/com/bitcodex/config/security/SecurityConfig.java
@@ -9,9 +9,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +21,9 @@ public class SecurityConfig {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -44,7 +49,9 @@ public class SecurityConfig {
 		
 		http.csrf(csrf->csrf.disable())
 		.authorizeHttpRequests(req-> req.requestMatchers("/api/v1/home/**","/api/v1/auth/**").permitAll().anyRequest().authenticated())
-		.httpBasic(Customizer.withDefaults());
+		.httpBasic(Customizer.withDefaults())
+		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
